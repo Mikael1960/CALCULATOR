e48b55ec8f2118aafd11f9b466049a5fe0c8e489
USER <USER@DESKTOP-DHKQ3S6>
2024-08-28 13:40:01 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtout = New TextBox()
+        lblname = New Label()
+        cmdclear = New Button()
+        cmdDiv = New Button()
+        cmdSev = New Button()
+        cmdEight = New Button()
+        cmdNine = New Button()
+        cmdPlus = New Button()
+        cmdFour = New Button()
+        cmdFive = New Button()
+        cmdSix = New Button()
+        cmdMinus = New Button()
+        cmdOne = New Button()
+        cmdTwo = New Button()
+        cmdThree = New Button()
+        cmdZero = New Button()
+        cmdDot = New Button()
+        cmdEqual = New Button()
+        cmdmulti = New Button()
+        SuspendLayout()
+        ' 
+        ' txtout
+        ' 
+        txtout.BackColor = SystemColors.Window
+        txtout.Font = New Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        txtout.ForeColor = SystemColors.WindowText
+        txtout.Location = New Point(22, 22)
+        txtout.Name = "txtout"
+        txtout.Size = New Size(235, 22)
+        txtout.TabIndex = 0
+        ' 
+        ' lblname
+        ' 
+        lblname.AutoSize = True
+        lblname.Font = New Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblname.ForeColor = SystemColors.ControlLightLight
+        lblname.Location = New Point(190, 48)
+        lblname.Name = "lblname"
+        lblname.Size = New Size(67, 16)
+        lblname.TabIndex = 1
+        lblname.Text = "CAT tech "
+        ' 
+        ' cmdclear
+        ' 
+        cmdclear.BackColor = SystemColors.ActiveCaption
+        cmdclear.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdclear.Location = New Point(22, 79)
+        cmdclear.Name = "cmdclear"
+        cmdclear.Size = New Size(116, 38)
+        cmdclear.TabIndex = 2
+        cmdclear.Text = "CLEAR"
+        cmdclear.UseVisualStyleBackColor = False
+        ' 
+        ' cmdDiv
+        ' 
+        cmdDiv.BackColor = SystemColors.GradientActiveCaption
+        cmdDiv.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdDiv.Location = New Point(214, 81)
+        cmdDiv.Name = "cmdDiv"
+        cmdDiv.Size = New Size(43, 38)
+        cmdDiv.TabIndex = 4
+        cmdDiv.Text = "/"
+        cmdDiv.UseVisualStyleBackColor = False
+        ' 
+        ' cmdSev
+        ' 
+        cmdSev.BackColor = SystemColors.ActiveCaption
+        cmdSev.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdSev.Location = New Point(22, 129)
+        cmdSev.Name = "cmdSev"
+        cmdSev.Size = New Size(43, 46)
+        cmdSev.TabIndex = 5
+        cmdSev.Text = "7"
+        cmdSev.UseVisualStyleBackColor = False
+        ' 
+        ' cmdEight
+        ' 
+        cmdEight.BackColor = SystemColors.ActiveCaption
+        cmdEight.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdEight.Location = New Point(92, 129)
+        cmdEight.Name = "cmdEight"
+        cmdEight.Size = New Size(43, 46)
+        cmdEight.TabIndex = 6
+        cmdEight.Text = "8"
+        cmdEight.UseVisualStyleBackColor = False
+        ' 
+        ' cmdNine
+        ' 
+        cmdNine.BackColor = SystemColors.ActiveCaption
+        cmdNine.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdNine.Location = New Point(157, 129)
+        cmdNine.Name = "cmdNine"
+        cmdNine.Size = New Size(43, 46)
+        cmdNine.TabIndex = 7
+        cmdNine.Text = "9"
+        cmdNine.UseVisualStyleBackColor = False
+        ' 
+        ' cmdPlus
+        ' 
+        cmdPlus.BackColor = SystemColors.GradientActiveCaption
+        cmdPlus.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdPlus.Location = New Point(214, 132)
+        cmdPlus.Name = "cmdPlus"
+        cmdPlus.Size = New Size(43, 46)
+        cmdPlus.TabIndex = 8
+        cmdPlus.Text = "+"
+        cmdPlus.UseVisualStyleBackColor = False
+        ' 
+        ' cmdFour
+        ' 
+        cmdFour.BackColor = SystemColors.ActiveCaption
+        cmdFour.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdFour.Location = New Point(22, 184)
+        cmdFour.Name = "cmdFour"
+        cmdFour.Size = New Size(43, 46)
+        cmdFour.TabIndex = 9
+        cmdFour.Text = "4"
+        cmdFour.UseVisualStyleBackColor = False
+        ' 
+        ' cmdFive
+        ' 
+        cmdFive.BackColor = SystemColors.ActiveCaption
+        cmdFive.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdFive.Location = New Point(92, 187)
+        cmdFive.Name = "cmdFive"
+        cmdFive.Size = New Size(43, 46)
+        cmdFive.TabIndex = 10
+        cmdFive.Text = "5"
+        cmdFive.UseVisualStyleBackColor = False
+        ' 
+        ' cmdSix
+        ' 
+        cmdSix.BackColor = SystemColors.ActiveCaption
+        cmdSix.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdSix.Location = New Point(157, 187)
+        cmdSix.Name = "cmdSix"
+        cmdSix.Size = New Size(43, 46)
+        cmdSix.TabIndex = 11
+        cmdSix.Text = "6"
+        cmdSix.UseVisualStyleBackColor = False
+        ' 
+        ' cmdMinus
+        ' 
+        cmdMinus.BackColor = SystemColors.GradientActiveCaption
+        cmdMinus.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdMinus.Location = New Point(214, 187)
+        cmdMinus.Name = "cmdMinus"
+        cmdMinus.Size = New Size(43, 46)
+        cmdMinus.TabIndex = 12
+        cmdMinus.Text = "-"
+        cmdMinus.UseVisualStyleBackColor = False
+        ' 
+        ' cmdOne
+        ' 
+        cmdOne.BackColor = SystemColors.ActiveCaption
+        cmdOne.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdOne.Location = New Point(22, 250)
+        cmdOne.Name = "cmdOne"
+        cmdOne.Size = New Size(43, 46)
+        cmdOne.TabIndex = 13
+        cmdOne.Text = "1"
+        cmdOne.UseVisualStyleBackColor = False
+        ' 
+        ' cmdTwo
+        ' 
+        cmdTwo.BackColor = SystemColors.ActiveCaption
+        cmdTwo.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdTwo.Location = New Point(92, 250)
+        cmdTwo.Name = "cmdTwo"
+        cmdTwo.Size = New Size(43, 46)
+        cmdTwo.TabIndex = 14
+        cmdTwo.Text = "2"
+        cmdTwo.UseVisualStyleBackColor = False
+        ' 
+        ' cmdThree
+        ' 
+        cmdThree.BackColor = SystemColors.ActiveCaption
+        cmdThree.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdThree.Location = New Point(157, 250)
+        cmdThree.Name = "cmdThree"
+        cmdThree.Size = New Size(43, 46)
+        cmdThree.TabIndex = 15
+        cmdThree.Text = "3"
+        cmdThree.UseVisualStyleBackColor = False
+        ' 
+        ' cmdZero
+        ' 
+        cmdZero.BackColor = SystemColors.ActiveCaption
+        cmdZero.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdZero.Location = New Point(22, 332)
+        cmdZero.Name = "cmdZero"
+        cmdZero.Size = New Size(116, 38)
+        cmdZero.TabIndex = 16
+        cmdZero.Text = "0"
+        cmdZero.UseVisualStyleBackColor = False
+        ' 
+        ' cmdDot
+        ' 
+        cmdDot.BackColor = SystemColors.ActiveCaption
+        cmdDot.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdDot.Location = New Point(157, 332)
+        cmdDot.Name = "cmdDot"
+        cmdDot.Size = New Size(43, 40)
+        cmdDot.TabIndex = 17
+        cmdDot.Text = "."
+        cmdDot.UseVisualStyleBackColor = False
+        ' 
+        ' cmdEqual
+        ' 
+        cmdEqual.BackColor = SystemColors.GradientActiveCaption
+        cmdEqual.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdEqual.Location = New Point(214, 250)
+        cmdEqual.Name = "cmdEqual"
+        cmdEqual.Size = New Size(43, 123)
+        cmdEqual.TabIndex = 18
+        cmdEqual.Text = "="
+        cmdEqual.UseVisualStyleBackColor = False
+        ' 
+        ' cmdmulti
+        ' 
+        cmdmulti.BackColor = SystemColors.GradientActiveCaption
+        cmdmulti.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold)
+        cmdmulti.Location = New Point(157, 79)
+        cmdmulti.Name = "cmdmulti"
+        cmdmulti.Size = New Size(43, 38)
+        cmdmulti.TabIndex = 3
+        cmdmulti.Text = "X"
+        cmdmulti.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Highlight
+        ClientSize = New Size(292, 450)
+        Controls.Add(cmdEqual)
+        Controls.Add(cmdDot)
+        Controls.Add(cmdZero)
+        Controls.Add(cmdThree)
+        Controls.Add(cmdTwo)
+        Controls.Add(cmdOne)
+        Controls.Add(cmdMinus)
+        Controls.Add(cmdSix)
+        Controls.Add(cmdFive)
+        Controls.Add(cmdFour)
+        Controls.Add(cmdPlus)
+        Controls.Add(cmdNine)
+        Controls.Add(cmdEight)
+        Controls.Add(cmdSev)
+        Controls.Add(cmdDiv)
+        Controls.Add(cmdmulti)
+        Controls.Add(cmdclear)
+        Controls.Add(lblname)
+        Controls.Add(txtout)
+        Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Name = "Form1"
+        Text = "CAT tech Calculator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtout As TextBox
+    Friend WithEvents lblname As Label
+    Friend WithEvents cmdclear As Button
+    Friend WithEvents cmdDiv As Button
+    Friend WithEvents cmdSev As Button
+    Friend WithEvents cmdEight As Button
+    Friend WithEvents cmdNine As Button
+    Friend WithEvents cmdPlus As Button
+    Friend WithEvents cmdFour As Button
+    Friend WithEvents cmdFive As Button
+    Friend WithEvents cmdSix As Button
+    Friend WithEvents cmdMinus As Button
+    Friend WithEvents cmdOne As Button
+    Friend WithEvents cmdTwo As Button
+    Friend WithEvents cmdThree As Button
+    Friend WithEvents cmdZero As Button
+    Friend WithEvents cmdDot As Button
+    Friend WithEvents cmdEqual As Button
+    Friend WithEvents cmdmulti As Button
+
+End Class
